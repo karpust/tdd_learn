d0e8247abe1f42c1166b40e170aca764b68ee878
Nchameleo <956277@mail.ru>
2025-01-26 10:18:43 +0300

    Не тестируем константы, тестирование html руками

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 import unittest
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,8 +20,36 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже"""
 
+        # Эдит слышала про крутое новое онлайн-приложение со списком
+        # неотложных дел. Она решает оценить его домашнюю страницу
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+
+        # Она видит, что заголовок и шапка страницы говорят о списках
+        # неотложных дел:
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Она набирает в текстовом поле "Купить павлиньи перья"
+        inputbox.send_keys('Купить павлиньи перья')  # вводит
+        inputbox.send_keys(Keys.ENTER)  # Keys съест и ctrl
+        time.sleep(1)  # явное ожидание - гарантирует, что браузер закончил загружать новую страницу,
+
+        # Когда она нажимает enter, страница обновляется, и теперь страница
+        # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
         self.fail('Закончить тест!')
 
 
--- tdd_learn_dj/lists/tests.py
+++ tdd_learn_dj/lists/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
+
 
 
 # Create your tests here.
@@ -10,6 +12,19 @@ class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         """тест: корневой url преобразуется в представление домашней страницы"""
-        pass
-        # found = resolve('/')
-        # self.assertEqual(found.func, home_page)
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """тест: домашняя страница возвращает правильный html"""
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
+        # сравниваем то что вернет вью и то что сделали вручную:
+        self.assertEqual(html, expected_html)
+
+        # а не тестируем константы - элементы html это и есть константы:
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>', html)
+        # self.assertTrue(html.endswith('</html>'))

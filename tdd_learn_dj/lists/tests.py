871efb71ccf5733be02c82a22515e74506268cd8
Nchameleo <956277@mail.ru>
2025-01-26 15:02:27 +0300

    тестируем не константы а логику, из двух тестов один

--- tdd_learn_dj/lists/tests.py
+++ tdd_learn_dj/lists/tests.py
@@ -13,20 +13,10 @@ from django.template.loader import render_to_string
 class HomePageTest(TestCase):
     """тест домашней страницы"""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """тест: корневой url преобразуется в представление домашней страницы"""
-        found = resolve('/')  # по урлу находит функцию представления
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         """тест: домашняя страница возвращает правильный html"""
+
+        # тестируем не константы а логику:
         response = self.client.get("/")
-        html = response.content.decode('utf8')
-
-        # а не тестируем константы - элементы html это и есть константы:
-        self.assertTrue(html.startswith('<html'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-
         self.assertTemplateUsed(response=response, template_name='home.html')  # работает только для self.client
 

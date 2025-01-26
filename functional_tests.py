0ab8c293c5ed7a4d4cb855f17d0236022115c85d
Nchameleo <956277@mail.ru>
2025-01-26 19:34:26 +0300

    Пока показывает только один элемент списка + рефакторинг кода функ теста

--- functional_tests.py
+++ functional_tests.py
@@ -20,6 +20,12 @@ class NewVisitorTest(unittest.TestCase):
         """демонтаж"""
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        '''подтверждение строки в таблице списка'''
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже"""
 
@@ -37,36 +43,26 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+            'Enter a to-do item')
 
-        # Она набирает в текстовом поле "Купить павлиньи перья"
-        inputbox.send_keys('Купить павлиньи перья')  # вводит
-        inputbox.send_keys(Keys.ENTER)  # Keys съест и ctrl
-        time.sleep(1)  # явное ожидание - гарантирует, что браузер закончил загружать новую страницу,
-
+        inputbox.send_keys('Купить павлиньи перья')
         # Когда она нажимает enter, страница обновляется, и теперь страница
-        # содержит "1: Купить павлиньи перья" в качестве элемента списка
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        # self.assertTrue(
-        # any(row.text == '1: Купить павлиньи перья' for row in rows),
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows]),
-        f'Новый элемент списка не появился в таблице. Содержимым было:\n{table.text}'
+        # содержит "1: Купить павлиньи перья" в качестве элемента таблицы списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        # Текствое поле по-прежнему приглашает ее добавить еще один элемент.
+        # Она вводит "Сделать мушку из павлиньих перьев"
+        # (Эдит очень методична)
 
-        # Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она
-        # вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+
         # Страница снова обновляется и теперь показывает оба элемента ее списка
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(
-            '1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn(
-            '2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         self.fail('Закончить тест!')
 

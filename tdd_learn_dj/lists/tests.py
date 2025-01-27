0ac588038069ceec7cd02e612aae8bbfa48fe6cd
Nchameleo <956277@mail.ru>
2025-01-27 11:25:54 +0300

    тест сохранения данных из пост-запроса, нужен рефакторинг, см комментарии

--- tdd_learn_dj/lists/tests.py
+++ tdd_learn_dj/lists/tests.py
@@ -7,6 +7,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 
 # Create your tests here.
@@ -35,4 +36,40 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
+
+class ItemModelTest(TestCase):
+    """тест модели элемента списка"""
+
+    def test_saving_and_retrieving_items(self):
+        """тест сохранения и получения элементов списка"""
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить post-запрос"""
+
+        # Не сохранять пустые элементы для каждого запроса
+        # Код с душком: тест POST-запроса слишком длинный?
+        # Показывать несколько элементов в таблице
+        # Поддержка более чем одного списка!
+
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
--- tdd_learn_dj/lists/views.py
+++ tdd_learn_dj/lists/views.py
@@ -1,16 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from lists.models import Item
+
 
 # Create your views here.
 
 
 def home_page(request):
-    # return render(request, 'home.html')  # создаст HttpResponse
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    # return render(request, 'home.html')
-
+    item = Item()
+    item.text = request.POST.get('item_text', '')  # сохраниение пустых полей с каждым запросом
+    item.save()
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
     })

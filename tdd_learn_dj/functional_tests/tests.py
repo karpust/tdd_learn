781c5b30a7358c6f014e1d104fa872eebb25b058
Nchameleo <956277@mail.ru>
2025-01-29 00:49:52 +0300

    новый красный тест

--- tdd_learn_dj/functional_tests/tests.py
+++ tdd_learn_dj/functional_tests/tests.py
@@ -77,6 +77,49 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.fail('Закончить тест!')
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        """тест: многочисленные пользователи могут начать списки по разным url"""
+
+        # Эдит начинает новый список
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+        # Она замечает, что ее список имеет уникальный URL-адрес
+        edith_list_url = self.browser.current_url
+
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Теперь новый пользователь, Фрэнсис, приходит на сайт.
+        ## Мы используем новый сеанс браузера, тем самым обеспечивая, чтобы никакая
+        ## информация от Эдит не прошла через данные cookie и пр.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        # Фрэнсис посещает домашнюю страницу. Нет никаких признаков списка Эдит
+        self.browser.get(self.live_server_url)
+
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Сделать мушку', page_text)
+
+        # Фрэнсис начинает новый список, вводя новый элемент. Он менее
+        # интересен, чем список Эдит...
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Купить молоко')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+        # Фрэнсис получает уникальный URL-адрес
+        francis_list_url = self.browser.current_url
+
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        # Опять-таки, нет ни следа от списка Эдит
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Купить молоко', page_text)
+
 
 # if __name__ == '__main__':  # тесты настроены на запуск джангой
 #     unittest.main(warnings='ignore')

d963b1564db6707953b7d7efc791fa19bbc11693
Nchameleo <956277@mail.ru>
2025-02-09 15:58:17 +0300

    рефакторинг, создание своих урлов приложению lists и подключение их в основной urls.py

--- tdd_learn_dj/lists/urls.py
+++ tdd_learn_dj/lists/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from lists import views
-from django.conf.urls import include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', views.home_page, name='home'),
-    path('lists/', include('lists.urls')),
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.new_item, name='new_item'),
 ]

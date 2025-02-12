37297e3ffe232d18e33a478c0ea6cfd0d443047f
Nchameleo <956277@mail.ru>
2025-02-12 20:02:37 +0300

    перемещение sqlite на один уровень с проектом

--- tdd_learn_dj/tdd_learn_dj/settings.py
+++ tdd_learn_dj/tdd_learn_dj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'tdd_learn_dj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 

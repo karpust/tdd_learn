376ad3dff22cb6b664d8a0acac9ab3d63a226400
Nchameleo <956277@mail.ru>
2025-02-22 17:16:26 +0300

    настраиваю запуск виртуального дисплея перед выполнением функц тестов, попытка 5 добавила локацию лисы в options

--- tdd_learn_dj/functional_tests/tests.py
+++ tdd_learn_dj/functional_tests/tests.py
@@ -11,8 +11,6 @@ from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 import os
-
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options
 
 
@@ -24,7 +22,7 @@ class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         """установка"""
         options = Options()
-        options.binary_location = '/snap/bin/firefox'  # for ubuntu-server
+        options.binary_location = '/usr/bin/firefox'  # for ubuntu-server
         self.browser = webdriver.Firefox(options=options)
         staging_server = os.getenv('STAGING_SERVER')
         if staging_server:

3afe7c2695e62038ff6eab69f70b4265054c73e6
Nchameleo <956277@mail.ru>
2025-02-23 15:08:31 +0300

    пытаюсь запустить тесты с гуглхромом и хромдрайвером

--- tdd_learn_dj/functional_tests/tests.py
+++ tdd_learn_dj/functional_tests/tests.py
@@ -18,8 +18,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class NewVisitorTest(LiveServerTestCase):
-# class NewVisitorTest(StaticLiveServerTestCase):
+# class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """тест нового посетителя"""
     MAX_WAIT = 10
 

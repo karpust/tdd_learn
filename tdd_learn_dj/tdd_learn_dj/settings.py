375791eed94da7f7d91c28239ed6975c741d073d
Nchameleo <956277@mail.ru>
2025-02-27 12:38:54 +0300

    добавлен allowed hosts

--- tdd_learn_dj/tdd_learn_dj/settings.py
+++ tdd_learn_dj/tdd_learn_dj/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver', '95.31.180.169']
 
 
 # Application definition
